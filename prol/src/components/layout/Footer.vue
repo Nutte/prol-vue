c709f756df76c105a230df70be2469c1fd62a0da
PunyFlash <puny.flash@gmail.com>
2020-01-27 23:00:14 +0200

    Add vue build

---
+++ prol/src/components/layout/Footer.vue
@@ -0,0 +1,134 @@
+<template>
+    <footer class="page-footer font-small blue pt-4">
+
+        <a v-on:click.prevent="() => scrollToTop(300)" id="go-top"><i class="fa fa-angle-up"></i></a>
+
+        <!-- Footer Links -->
+        <div class="container-fluid footer-conteiner text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row">
+
+
+                <!-- Grid column -->
+                <div class="col-md-3 mt-md-0 mt-3">
+
+                    <!-- Content -->
+                    <a class="navbar-brand" href="#">
+                        <img src="./../../assets/img/logo.png" width="150" height="150" class="d-inline-block align-top" alt="">
+                    </a>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">
+                        МЫ В СОЦСЕТЯХ</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">facebook</a>
+                        </li>
+                        <li>
+                            <a href="#!">instagram</a>
+                        </li>
+                        <li>
+                            <a href="#!">youtube</a>
+                        </li>
+                        <li>
+                            <a href="#!">vkontakte</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">НАШИ ПАРТНЕРЫ</h5>
+
+                    <img src="./../../assets/img/footer-partner.png" width="200" height="60" class="d-inline-block align-top" alt="">
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">КРЕАТИВНЫЙ ПАРТНЕР</h5>
+
+                    <!-- Content -->
+                    <a class="navbar-brand" href="#">
+                        <img src="./../../assets/img/logo-part.png" width="150" height="30" class="d-inline-block align-top" alt="">
+                    </a>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright  py-3"> <a href="#"> Developed by Lights.agency </a> | all rights reserved ©
+
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'footer',
+        methods: {
+            topButton() {
+                const doc = document.documentElement;
+                const st = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+                var btn = document.getElementById("go-top");
+
+                if(st > 300) {
+                    if( !btn.classList.contains("show") ) {
+                        btn.classList.add("show");
+                    }
+                }  
+                else {
+                    if( btn.classList.contains("show") ) {
+                        btn.classList.remove("show");
+                    }
+                }
+            },
+            scrollToTop(scrollDuration) {
+                let scrollHeight = window.scrollY,
+                    scrollStep = Math.PI / ( scrollDuration / 15 ),
+                    cosParameter = scrollHeight / 2;
+
+                let scrollCount = 0,
+                    scrollMargin,
+                    scrollInterval = setInterval( () => {
+                        if ( window.scrollY !== 0 ) {
+                            scrollCount = scrollCount + 1;
+                            scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+                            window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+                        }
+                        else clearInterval(scrollInterval);
+                    }, 15 );
+            }
+        },
+        mounted() {
+            window.addEventListener("scroll", this.topButton);
+        },
+        beforeDestroy() {
+            window.removeEventListener("scroll", this.topButton);
+        }
+    }
+</script>
---
+++ prol/src/components/layout/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <nav class=" navbar navbar-expand-lg fixed-top" id="heder">
+
+        <router-link class="navbar-brand" to="/">
+            <img src="./../../assets/img/logo.png" width="100" height="100" class="d-inline-block align-top" alt="">
+        </router-link>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            &#8212;
+            &#8212;
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  mr-left">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="calendar">РАСПИСАНИЕ ИГР </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="tour">ТУРНИРЫ</router-link>
+                </li>
+                <li class="nav-item dropdown">
+                    <router-link class="nav-link" to="glory-hall">
+                        ЗАЛ СЛАВЫ
+                    </router-link>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">КИЛЛЫ</a>
+
+                        <a class="dropdown-item" href="#">АССИСТЫ</a>
+
+                        <a class="dropdown-item" href="#">СМЕРТИ</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="news">НОВОСТИ</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="login">ВХОД</router-link>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+
+    <!-- header -->
+
+</template>
+
+<script>
+    export default {
+        name: 'header',
+        methods: {
+            handleScroll() {
+                const doc = document.documentElement;
+                const st = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+                let header = document.getElementById("heder");
+
+                if(st >= 50) {
+                    if( !header.classList.contains("navSkrol") ) {
+                        header.classList.add("navSkrol");
+                    }
+                }  
+                else {
+                    if( header.classList.contains("navSkrol") ) {
+                        header.classList.remove("navSkrol");
+                    }
+                }
+            }
+        },
+        mounted() {
+            window.addEventListener("scroll", this.handleScroll);
+        },
+        beforeDestroy() {
+            window.removeEventListener("scroll", this.handleScroll);
+        }
+    }
+</script>
